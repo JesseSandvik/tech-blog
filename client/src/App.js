2661b55e98318b96b79b60d98f7f8c3332965c3f
Jesse Sandvik <jsandvik89@gmail.com>
2022-07-09 15:23:32 -0400

    basic styling folder structure for scss/css created, basic styling applied

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,30 @@
+import Footer from "./components/organisms/footer/Footer";
+import Header from "./components/organisms/header/Header";
+import Heading from "./components/atoms/heading/Heading";
+
 function App() {
-  return <div>App</div>;
+  const darkTheme = {
+    background: "#0f0f0f",
+    color: "#f0f0f0",
+  };
+
+  const lightTheme = {
+    background: "#f0f0f0",
+    color: "$0f0f0f",
+  };
+
+  return (
+    <div style={darkTheme} id="app">
+      <Header>
+        <Heading level="1">tech blog</Heading>
+      </Header>
+      <Footer>
+        <Heading level="5">
+          &copy; jessesandvik {new Date().getFullYear()}
+        </Heading>
+      </Footer>
+    </div>
+  );
 }
 
 export default App;
