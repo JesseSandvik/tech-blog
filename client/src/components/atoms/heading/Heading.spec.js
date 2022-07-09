cfc1fcaac2f45111f6cd7dda20b959ba4c0ae877
Jesse Sandvik <jsandvik89@gmail.com>
2022-07-08 21:54:45 -0400

    added additional tests for heading component

--- client/src/components/atoms/heading/Heading.spec.js
+++ client/src/components/atoms/heading/Heading.spec.js
@@ -3,12 +3,28 @@ import Heading from './Heading';
 
 describe('<Heading />', () => {
   test('01. Renders the Heading component', () => {
-    render(<Heading level='2'>testing</Heading>);
+    render(<Heading>testing</Heading>);
 
-    const headingLevelTwo = screen.getByRole('heading', {
+    const heading = screen.getByRole('heading', {
       name: /testing/i,
     });
 
-    expect(headingLevelTwo).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
+  });
+  test('02. Heading component renders even if level prop is not provided', () => {
+    render(<Heading>whoops</Heading>);
+
+    const heading = screen.getByRole('heading', { name: /whoops/i });
+
+    expect(heading).toBeInTheDocument();
+  });
+  test('03. Heading component renders the children prop', () => {
+    render(<Heading>I am a child prop</Heading>);
+
+    const heading = screen.getByRole('heading', {
+      name: /I am a child prop/i,
+    });
+
+    expect(heading).toBeInTheDocument();
   });
 });
